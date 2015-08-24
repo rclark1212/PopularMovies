17493000becf675c8924a9815ec8ac7af4c8fcdf
rclark1212 <rclark1212@gmail.com>
2015-08-23 18:26:25 -0700

    change name of fragment to ShowMoviesFragment

--- app/src/main/java/app/com/example/android/popularmovies/ShowMoviesFragment.java
+++ app/src/main/java/app/com/example/android/popularmovies/ShowMoviesFragment.java
@@ -9,9 +9,9 @@ import android.view.ViewGroup;
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class ShowMoviesFragment extends Fragment {
 
-    public MainActivityFragment() {
+    public ShowMoviesFragment() {
     }
 
     @Override

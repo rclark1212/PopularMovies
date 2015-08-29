c9b694c7defc07cc2023f774c51679c4bb58a674
rclark1212 <rclark1212@gmail.com>
2015-08-29 13:06:14 -0700

    Implement grid view and add detail fragment view navigation. Settings are not done. Only tested on phone.

--- app/src/main/java/app/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/app/com/example/android/popularmovies/MainActivity.java
@@ -1,9 +1,33 @@
 package app.com.example.android.popularmovies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+/*  OK - summary of project
+    1) Supports phone/tablet (ideally ATV but lets see)
+    2) Main activity will show gridview of movies ordered by popularity, rating, date
+    or favorites (seems like hiding this on settings screen would be dumb)
+    3) Settings screen used to control ordering
+    4) Settings screen also controls how much data (could also do a more button - TBD)
+    5) Clicking on movie brings up data/details of movie. There will also be a "More..."
+        button which will invoke a search intent
+        a) Phone - detail screen. Will be full screen normal fragment
+        b) Tablet - detail screen will be a popup. Re-use fragment as dialog...
+        c) Both UIs will also have a trailers option and a reviews text box. As well as
+        ability to mark/store as favorite locally
+
+    Steps:
+    1) flesh out all primary nav/screens
+    2) add primary data elements like grid view with dummy data on phone
+    3) Implement tablet for above
+    4) flesh out additional control elements
+    5) implement data population/helper libs
+    6) refactor as necessary
+    7) tweak UI
+ */
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +54,10 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            //launch settings
+            /* TODO
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent); */
             return true;
         }
 
--- app/src/main/java/app/com/example/android/popularmovies/ShowMoviesFragment.java
+++ app/src/main/java/app/com/example/android/popularmovies/ShowMoviesFragment.java
@@ -1,15 +1,34 @@
 package app.com.example.android.popularmovies;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * A placeholder fragment containing a simple view.
+ * This fragment will populate, show and respond to the main movies screen
  */
 public class ShowMoviesFragment extends Fragment {
+
+    private ArrayAdapter<String> m_my_array_adapter;
+    public final static String EXTRA_MESSAGE = "app.com.example.android.popularmovies.MESSAGE";
+    GridView m_grid;
+    //TODO
+    private static final String[] TestStr = new String[] {
+            "A", "B", "C", "D", "E",
+            "F", "G", "H", "I", "J",
+            "K", "L", "M", "N", "O",
+            "P", "Q", "R", "S", "T",
+            "U", "V", "W", "X", "Y", "Z"};
 
     public ShowMoviesFragment() {
     }
@@ -17,6 +36,29 @@ public class ShowMoviesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_movies, container, false);
+        View retView = inflater.inflate(R.layout.fragment_movies, container, false);
+        this.setHasOptionsMenu(true);
+
+        //find the grid control
+        m_grid = (GridView) retView.findViewById(R.id.gridview_movies);
+
+        //set the adapter. Note that ImageAdapter is a custom class
+        m_grid.setAdapter(new ImageAdapter(getActivity(), TestStr));
+
+        //and listen for a click
+        m_grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            //TODO
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //once a grid item is clicked, open up the detail view
+                Intent intent = new Intent(getActivity(), DetailActivity.class);
+                String message = ((TextView) view.findViewById(R.id.grid_item_label)).getText().toString();
+                intent.putExtra(EXTRA_MESSAGE, message + " " + message); //TODO
+                startActivity(intent);
+            }
+        });
+
+        return retView;
     }
+
 }
